a1d00bda68aa05d45590dc659a38ab7949adc30a
Ruslan Duadze <ruslankavaziduadze@gmail.com>
2026-07-13 12:20:11 +0400

    Add Log4j2 console and rolling file logging

--- src/test/java/pages/SecureAreaPage.java
+++ src/test/java/pages/SecureAreaPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,30 +8,51 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class SecureAreaPage extends AbstractPage {
 
-  @FindBy(css = "h2")
-  private WebElement secureAreaTitle;
+  private static final By SECURE_AREA_TITLE =
+      By.cssSelector("h2");
+
+  private static final By FLASH_MESSAGE =
+      By.id("flash");
 
   @FindBy(css = ".button.secondary.radius")
   private WebElement logoutButton;
-
-  @FindBy(css = "#flash")
-  private WebElement flashMessage;
 
   public SecureAreaPage(WebDriver driver) {
     super(driver);
   }
 
   public boolean isPageOpened() {
-    return wait.until(ExpectedConditions.visibilityOf(secureAreaTitle)).isDisplayed()
-        && secureAreaTitle.getText().equals("Secure Area");
+    wait.until(
+        ExpectedConditions.urlContains("/secure")
+    );
+
+    return wait.until(
+        ExpectedConditions.textToBe(
+            SECURE_AREA_TITLE,
+            "Secure Area"
+        )
+    );
   }
 
   public String getFlashMessageText() {
-    return wait.until(ExpectedConditions.visibilityOf(flashMessage)).getText();
+    return wait.until(
+        ExpectedConditions.visibilityOfElementLocated(
+            FLASH_MESSAGE
+        )
+    ).getText();
   }
 
   public LoginPage logout() {
-    logoutButton.click();
+    wait.until(
+        ExpectedConditions.elementToBeClickable(
+            logoutButton
+        )
+    ).click();
+
+    wait.until(
+        ExpectedConditions.urlContains("/login")
+    );
+
     return new LoginPage(driver);
   }
 }
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,22 +2,35 @@ package tests;
 
 import driver.DriverFactory;
 import driver.DriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public abstract class BaseTest {
 
+  private static final Logger LOGGER =
+      LogManager.getLogger(BaseTest.class);
+
   protected WebDriver driver;
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() {
+    LOGGER.info("Starting test setup");
+
     DriverFactory.createDriver();
     driver = DriverManager.getDriver();
+
+    LOGGER.info("WebDriver has been initialized");
   }
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() {
+    LOGGER.info("Starting test cleanup");
+
     DriverManager.quitDriver();
+
+    LOGGER.info("WebDriver has been closed");
   }
 }
